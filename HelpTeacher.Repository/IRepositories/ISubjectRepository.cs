d13dc83d23f41dbfa52edf8b9b22109c1912921e
Otavio Bueno Silva <obsilva94@gmail.com>
2018-09-21 00:42:50 -0300

    Interface de repositório de assunto
    * Criação da interface para o repositório de assuntos.

---
+++ HelpTeacher.Repository/IRepositories/ISubjectRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+using HelpTeacher.Domain.Entities;
+
+namespace HelpTeacher.Repository.IRepositories
+{
+	public interface ISubjectRepository : IRepositoryBase<Subject>
+	{
+		#region Methods
+		/// <summary>Recupera todos os registros que possuem relação o objeto informado.</summary>
+		/// <param name="obj">Objeto que deve ser buscado.</param>
+		/// <returns>Todos os registros com relacionados ao objeto especificado.</returns>
+		IQueryable<Subject> GetWhereCourse(Discipline obj);
+
+		/// <summary>Recupera todos os registros que possuem relação o objeto de ID informado.</summary>
+		/// <param name="obj">ID do que deve ser buscado.</param>
+		/// <returns>Todos os registros com relacionados ao ID especificado.</returns>
+		IQueryable<Subject> GetWhereCourse(int id);
+
+		/// <summary>
+		/// Recupera todos os registros ativos ou não e que possuem relação o objeto informado.
+		/// </summary>
+		/// <param name="obj">Objeto que deve ser buscado.</param>
+		/// <param name="isRecordActive">Registro deve estar ativo?</param>
+		/// <returns>Todos os registros com relacionados ao objeto especificado.</returns>
+		IQueryable<Discipline> GetWhereCourse(Course obj, bool isRecordActive);
+
+		/// <summary>Recupera todos os registros ativos ou não e que possuem relação o objeto de ID informado.</summary>
+		/// <param name="obj">Objeto que deve ser buscado.</param>
+		/// /// <param name="isRecordActive">Registro deve estar ativo?</param>
+		/// <returns>Todos os registros com relacionados ao ID especificado.</returns>
+		IQueryable<Discipline> GetWhereCourse(int id, bool isRecordActive);
+		#endregion
+	}
+}
