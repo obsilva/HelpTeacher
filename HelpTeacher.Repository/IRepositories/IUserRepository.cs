e6082f8fa54b79c27c7e8cd2f7c1068536e150a0
Otavio Bueno Silva <obsilva94@gmail.com>
2018-09-23 23:12:57 -0300

    Interface de repositório de usuário
    * Criação da interface para o repositório de usuários.

---
+++ HelpTeacher.Repository/IRepositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿/* Authors: Otávio Bueno Silva <obsilva94@gmail.com>
+ * Since: 2018-09-23
+ */
+
+using HelpTeacher.Domain.Entities;
+
+namespace HelpTeacher.Repository.IRepositories
+{
+	/// <summary>Define o repositório de <see cref="User"/>.</summary>
+	public interface IUserRepository : IRepositoryBase<User>
+	{
+
+	}
+}
