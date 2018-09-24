1e1ea33b192d7ca4cf760e2bc7f71192b9de70e1
Otavio Bueno Silva <obsilva94@gmail.com>
2018-09-24 13:29:10 -0300

    Interface de repositório de avaliação
    * Criação da interface para o repositório de avaliações.

---
+++ HelpTeacher.Repository/IRepositories/IExamRepository.cs
@@ -0,0 +1,13 @@
+﻿/* Authors: Otávio Bueno Silva <obsilva94@gmail.com>
+ * Since: 2018-09-24
+ */
+
+using HelpTeacher.Domain.Entities;
+
+namespace HelpTeacher.Repository.IRepositories
+{
+	/// <summary>Define o repositório de <see cref="Exam"/>.</summary>
+	public interface IExamRepository : IRepositoryBase<Exam>
+	{
+	}
+}
